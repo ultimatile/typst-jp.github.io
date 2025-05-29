e3e351b32889eaea757e7dc38af8d9f5fa0e184a
gomazarashi <60730277+gomazarashi@users.noreply.github.com>
2025-05-29 14:59:04 +0900

    `/docs/reference/math/frac/`の翻訳 (#168)
    Co-authored-by: Shunsuke KIMURA <kimushun1101@gmail.com>

--- crates/typst-library/src/math/frac.rs
+++ crates/typst-library/src/math/frac.rs
@@ -4,26 +4,26 @@ use crate::diag::bail;
 use crate::foundations::{elem, Content, Value};
 use crate::math::Mathy;
 
-/// A mathematical fraction.
+/// 分数。
 ///
-/// # Example
+/// # 例
 /// ```example
 /// $ 1/2 < (x+1)/2 $
 /// $ ((x+1)) / 2 = frac(a, b) $
 /// ```
 ///
-/// # Syntax
-/// This function also has dedicated syntax: Use a slash to turn neighbouring
-/// expressions into a fraction. Multiple atoms can be grouped into a single
-/// expression using round grouping parenthesis. Such parentheses are removed
-/// from the output, but you can nest multiple to force them.
+/// # 構文
+/// この関数には専用の構文もあります。
+/// 隣接する式をスラッシュで区切ると、分数になります。
+/// また、丸括弧で複数の式要素を囲うと、単一の式として扱えます。
+/// そのような丸括弧は出力からは削除されますが、複数重ねてネストすることで、丸括弧を表示させることも可能です。
 #[elem(title = "Fraction", Mathy)]
 pub struct FracElem {
-    /// The fraction's numerator.
+    /// 分数の分子。
     #[required]
     pub num: Content,
 
-    /// The fraction's denominator.
+    /// 分数の分母。
     #[required]
     pub denom: Content,
 }
