20bf99ad4e6e32055c2a785020a797baa1a64945
ultimatile <ultimatile@users.noreply.github.com>
2025-07-13 23:54:32 +0900

    `/docs/reference/layout/place`の翻訳 (#209)
    Co-authored-by: Shunsuke KIMURA <kimushun1101@gmail.com>
    Co-authored-by: 3w36zj6 <52315048+3w36zj6@users.noreply.github.com>

--- crates/typst-library/src/layout/place.rs
+++ crates/typst-library/src/layout/place.rs
@@ -2,27 +2,20 @@ use crate::foundations::{elem, scope, Cast, Content, Packed, Smart};
 use crate::introspection::{Locatable, Unqueriable};
 use crate::layout::{Alignment, Em, Length, Rel};
 
-/// Places content relatively to its parent container.
+/// 親コンテナに対して相対的なコンテンツの配置。
 ///
-/// Placed content can be either overlaid (the default) or floating. Overlaid
-/// content is aligned with the parent container according to the given
-/// [`alignment`]($place.alignment), and shown over any other content added so
-/// far in the container. Floating content is placed at the top or bottom of
-/// the container, displacing other content down or up respectively. In both
-/// cases, the content position can be adjusted with [`dx`]($place.dx) and
-/// [`dy`]($place.dy) offsets without affecting the layout.
+/// コンテンツはオーバーレイ（デフォルト）またはフロートのいずれかで配置できます。
+/// オーバーレイのコンテンツは、指定された[`alignment`]($place.alignment)に従って親コンテナに合わせて配置され、これまでに追加された他のコンテンツの上に表示されます。
+/// フロートのコンテンツは、親コンテナの上部または下部に配置され、他のコンテンツを下または上にそれぞれずらします。
+/// 両方の場合で、コンテンツの位置は[`dx`]($place.dx)および[`dy`]($place.dy)オフセットを用いてレイアウトに影響を与えることなく調整可能です。
 ///
-/// The parent can be any container such as a [`block`], [`box`],
-/// [`rect`], etc. A top level `place` call will place content directly
-/// in the text area of the current page. This can be used for absolute
-/// positioning on the page: with a `top + left`
-/// [`alignment`]($place.alignment), the offsets `dx` and `dy` will set the
-/// position of the element's top left corner relatively to the top left corner
-/// of the text area. For absolute positioning on the full page including
-/// margins, you can use `place` in [`page.foreground`]($page.foreground) or
-/// [`page.background`]($page.background).
+/// [`block`]、[`box`]、[`rect`]などの任意のコンテナが親になることができます。
+/// トップレベルの`place`の呼び出しは現在のページのテキスト領域に直接コンテンツを配置します。
+/// これはページ上の絶対位置指定に使用できます。
+/// `top + left` [`alignment`]($place.alignment)と組み合わせると、`dx`および`dy`オフセットはテキスト領域の左上隅を基準として要素の左上隅の位置を設定します。
+/// マージンを含めたページ全体における絶対位置指定は、[`page.foreground`]か[`page.background`]中で`place`を使うと可能です。
 ///
-/// # Examples
+/// # 例
 /// ```example
 /// #set page(height: 120pt)
 /// Hello, world!
@@ -40,17 +33,14 @@ use crate::layout::{Alignment, Em, Length, Rel};
 /// )
 /// ```
 ///
-/// # Effect on the position of other elements { #effect-on-other-elements }
-/// Overlaid elements don't take space in the flow of content, but a `place`
-/// call inserts an invisible block-level element in the flow. This can
-/// affect the layout by breaking the current paragraph. To avoid this,
-/// you can wrap the `place` call in a [`box`] when the call is made
-/// in the middle of a paragraph. The alignment and offsets will then be
-/// relative to this zero-size box. To make sure it doesn't interfere with
-/// spacing, the box should be attached to a word using a word joiner.
+/// # 他の要素の位置に対する影響 { #effect-on-other-elements }
+/// 要素のオーバーレイはコンテンツの流れの中にスペースを取りませんが、`place`の呼び出しは、流れの中にブロックレベルの不可視要素を挿入します。
+/// これは現在の段落を中断することでレイアウトに影響を与える可能性があります。
+/// これを避けるために、段落の途中で`place`を呼び出す場合には、それを[`box`]でラップするとよいでしょう。
+/// このとき、配置とオフセットは、この大きさを持たないボックスを基準にしたものになります。
+/// 間隔に影響しないように、ワードジョイナーを使用してボックスを単語に結合してください。
 ///
-/// For example, the following defines a function for attaching an annotation
-/// to the following word:
+/// 例えば、以下は直後の単語に注釈を付与する関数を定義しています。
 ///
 /// ```example
 /// >>> #set page(height: 70pt)
@@ -64,32 +54,26 @@ use crate::layout::{Alignment, Em, Length, Rel};
 /// square in my text.
 /// ```
 ///
-/// The zero-width weak spacing serves to discard spaces between the function
-/// call and the next word.
+/// ゼロ幅の弱い空白は、関数呼び出しと次の単語との間の空白を削除する役割を果たします。
 #[elem(scope, Locatable, Unqueriable)]
 pub struct PlaceElem {
-    /// Relative to which position in the parent container to place the content.
+    /// コンテンツを配置する基準となる親コンテナ内の位置。
     ///
-    /// - If `float` is `{false}`, then this can be any alignment other than `{auto}`.
-    /// - If `float` is `{true}`, then this must be `{auto}`, `{top}`, or `{bottom}`.
+    /// - `float`が`{false}`の場合、`{auto}`以外の任意のalignmentを指定できます。
+    /// - `float`が`{true}`の場合、`{auto}`、`{top}`あるいは`{bottom}`のいずれかを指定しなければなりません。
     ///
-    /// When `float` is `{false}` and no vertical alignment is specified, the
-    /// content is placed at the current position on the vertical axis.
+    /// `float`が`{false}`で、かつvertical alignmentが指定されていなければ、コンテンツは垂直方向の軸上の現在の位置に配置されます。
     #[positional]
     #[default(Smart::Custom(Alignment::START))]
     pub alignment: Smart<Alignment>,
 
-    /// Relative to which containing scope something is placed.
+    /// 何かを配置するときの基準となる包含スコープ。
     ///
-    /// The parent scope is primarily used with figures and, for
-    /// this reason, the figure function has a mirrored [`scope`
-    /// parameter]($figure.scope). Nonetheless, it can also be more generally
-    /// useful to break out of the columns. A typical example would be to
-    /// [create a single-column title section]($guides/page-setup-guide/#columns)
-    /// in a two-column document.
+    /// 親スコープは主に図表に使用されるため、figure関数にはそれを反映した[`scope`パラメーター]($figure.scope)が用意されています。
+    /// しかしながら、段組を中断することは、より一般的な場合に有用であることもあります。
+    /// 典型的な例は2段組の文書で1段組のタイトル節を作成することでしょう。
     ///
-    /// Note that parent-scoped placement is currently only supported if `float`
-    /// is `{true}`. This may change in the future.
+    /// 現在、親スコープでの配置は`float`が`{true}`の場合のみサポートされています。 この挙動は将来変更される可能性があります。
     ///
     /// ```example
     /// #set page(height: 150pt, columns: 2)
@@ -104,12 +88,11 @@ pub struct PlaceElem {
     /// ```
     pub scope: PlacementScope,
 
-    /// Whether the placed element has floating layout.
+    /// 要素をフロートレイアウトで配置するかどうか。
     ///
-    /// Floating elements are positioned at the top or bottom of the parent
-    /// container, displacing in-flow content. They are always placed in the
-    /// in-flow order relative to each other, as well as before any content
-    /// following a later [`place.flush`] element.
+    /// フロートの要素は流れの中にあるコンテンツをずらして親コンテナの上部または下部に位置取ります。
+    ///
+    /// それらは常に互いのフロー内での順序を保ち、後続の[`place.flush`]要素に続くコンテンツより前に配置されます。
     ///
     /// ```example
     /// #set page(height: 150pt)
@@ -129,15 +112,15 @@ pub struct PlaceElem {
     /// ```
     pub float: bool,
 
-    /// The spacing between the placed element and other elements in a floating
-    /// layout.
+    /// フロートレイアウトでの配置された要素と他の要素との間隔。
     ///
-    /// Has no effect if `float` is `{false}`.
+    /// `float`が`{false}`の場合は影響がありません。
+
     #[default(Em::new(1.5).into())]
     #[resolve]
     pub clearance: Length,
 
-    /// The horizontal displacement of the placed content.
+    /// 配置したコンテンツの水平方向の変位。
     ///
     /// ```example
     /// #set page(height: 100pt)
@@ -147,19 +130,17 @@ pub struct PlaceElem {
     /// }
     /// ```
     ///
-    /// This does not affect the layout of in-flow content.
-    /// In other words, the placed content is treated as if it
-    /// were wrapped in a [`move`] element.
+    /// これは流れの中のコンテンツのレイアウトには影響しません。
+    /// 言い換えると、配置されたコンテンツは[`move`]要素にラップされたかのように扱われます。
     pub dx: Rel<Length>,
 
-    /// The vertical displacement of the placed content.
+    /// 配置したコンテンツの垂直方向の変位。
     ///
-    /// This does not affect the layout of in-flow content.
-    /// In other words, the placed content is treated as if it
-    /// were wrapped in a [`move`] element.
+    /// これは流れの中のコンテンツのレイアウトには影響しません。
+    /// 言い換えると、配置されたコンテンツは[`move`]要素にラップされたかのように扱われます。
     pub dy: Rel<Length>,
 
-    /// The content to place.
+    /// 配置するコンテンツ。
     #[required]
     pub body: Content,
 }
@@ -184,11 +165,9 @@ pub enum PlacementScope {
     Parent,
 }
 
-/// Asks the layout algorithm to place pending floating elements before
-/// continuing with the content.
+/// コンテンツの続行前に保留中のフロート要素を配置するようレイアウトアルゴリズムに指示。
 ///
-/// This is useful for preventing floating figures from spilling
-/// into the next section.
+/// これは、次のセクションにフロートの図表が流れ込むのを防ぐのに便利です。
 ///
 /// ```example
 /// >>> #set page(height: 160pt, width: 150pt)
