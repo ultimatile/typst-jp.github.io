51366496a3f2c50f2e148357bbd827a1ec6f8411
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2025-08-13 06:14:03 +0900

    chore: WebサイトのSitemapとrobots.txtの生成でHono SSG pluginを使用するように変更 (#253)

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,5 +1,7 @@
 import { rmSync, symlinkSync } from "node:fs";
 import { resolve } from "node:path";
+import { robotsTxtPlugin } from "@hono/ssg-plugins-essential/robots-txt";
+import { sitemapPlugin } from "@hono/ssg-plugins-essential/sitemap";
 import { defaultOptions } from "@hono/vite-dev-server";
 import devServer from "@hono/vite-dev-server";
 import ssg from "@hono/vite-ssg";
@@ -16,7 +18,17 @@ symlinkSync(assetsDocsPath, publicAssetsDocsPath, "dir");
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
-		ssg(),
+		ssg({
+			plugins: [
+				sitemapPlugin({
+					baseUrl: "https://typst-jp.github.io/",
+				}),
+				robotsTxtPlugin({
+					rules: [{ userAgent: "*", allow: ["/"] }],
+					sitemapUrl: "https://typst-jp.github.io/sitemap.xml",
+				}),
+			],
+		}),
 		devServer({
 			entry: "src/index.tsx",
 			exclude: [...defaultOptions.exclude, /^\/assets\/.+/, /^\/index\.html$/],
