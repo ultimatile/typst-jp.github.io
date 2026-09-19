44906b674af5424359626950f3c32ab81ea723f7
ultimatile <ultimatile@users.noreply.github.com>
2026-09-19 12:49:44 +0900

    `/docs/reference/foundations/datetime/`の再翻訳
    v0.14.1への更新で追加された`datetime.today`のCLIでの上書き方法の説明を翻訳。

--- crates/typst-library/src/foundations/datetime.rs
+++ crates/typst-library/src/foundations/datetime.rs
@@ -276,11 +276,10 @@ impl Datetime {
 
     /// 現在の日付を取得。
     ///
-    /// In the CLI, this can be overridden with the `--creation-timestamp`
-    /// argument or by setting the
-    /// [`SOURCE_DATE_EPOCH`](https://reproducible-builds.org/specs/source-date-epoch/)
-    /// environment variable. In both cases, the value should be given as a UNIX
-    /// timestamp.
+    /// CLIでは、`--creation-timestamp`引数を渡すか、
+    /// [`SOURCE_DATE_EPOCH`](https://reproducible-builds.org/specs/source-date-epoch/)環境変数を設定することで、
+    /// この値を上書きできます。
+    /// いずれの場合も、値はUNIXタイムスタンプで指定してください。
     ///
     /// ```example
     /// Today's date is
